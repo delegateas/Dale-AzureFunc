268c19b618ecd29f79e1c9ec3b88fd628f87b317
warreq <brandon.s.lucas@gmail.com>
2016-11-08 13:11:00 +0100

    Didn't need 'return'

--- Notify/run.fsx
+++ Notify/run.fsx
@@ -1,9 +1,8 @@
 open System.Net
 open System.Net.Http
-open FSharp.Interop.Dynamic
 open Newtonsoft.Json
 open Dale.Http
 
 let Run(req: HttpRequestMessage, log: TraceWriter) :Task<HttpResponseMessage> =
   log.Info(sprintf "WebHook triggered.")
-  return wrappedAuditHandler req |> Async.StartAsTask
+  wrappedAuditHandler req |> Async.StartAsTask
