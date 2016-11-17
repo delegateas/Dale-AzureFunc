115782069ab25a0bef5fae0e95dea84fcf330876
warreq <brandon.s.lucas@gmail.com>
2016-11-17 06:11:02 +0100

    Remove F# version

--- NotifyF/run.fsx
+++
@@ -1,8 +0,0 @@
-open System.Net
-open System.Net.Http
-open Newtonsoft.Json
-open Dale.Middleware
-
-let Run(req: HttpRequestMessage, log: TraceWriter) :Task<HttpResponseMessage> =
-  log.Info(sprintf "WebHook triggered.")
-  wrappedAuditHandler req |> Async.StartAsTask
